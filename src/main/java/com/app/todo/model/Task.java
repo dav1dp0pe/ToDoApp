2664d47a5d170c686677aac7794bb905b785ef05
dav1dp0pe <pope3705@gmail.com>
2025-12-17 17:40:16 -0500

    Updated Tasks.html, TaskController.java, Task.java, and application.properties files

--- src/main/java/com/app/todo/model/Task.java
+++ src/main/java/com/app/todo/model/Task.java
@@ -1,13 +1,11 @@
 package com.app.todo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "tasks")
 public class Task {
     @Id                                                 //ID
     @GeneratedValue(strategy = GenerationType.AUTO)     //has to autogenerate the id
