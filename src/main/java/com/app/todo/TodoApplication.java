aa3d952bf9189c0293029fbfae2d1787695c8403
dav1dp0pe <pope3705@gmail.com>
2025-12-17 15:27:10 -0500

    Initial Commit

---
+++ src/main/java/com/app/todo/TodoApplication.java
@@ -0,0 +1,15 @@
+package com.app.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//TODO FIX THE ID, TITLE, AND COMPLETED TO UPDATE TO THE DATABASE!
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
